79774060efaa6bc9672b67435db2184285c6ee88
Andres Cruz <andrescruz@Mac-mini-de-Andres.local>
2020-11-25 12:56:39 -0400

    App esqueleto en Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SendMsj/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SendMsj from './components/SendMsj'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SendMsj
   }
 }
 </script>
---
+++ src/components/SendMsj.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        {{msj}}
+    </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            msj:"Conectando..."
+        }
+    },
+
+    mounted() {
+        var url = 'ws://127.0.0.1:8000/ws/alert/room/3?Token_cf73616455a60301c8de852010634d12c134b0c7'
+        var alertSocket = new WebSocket(url)
+        const my = this
+        alertSocket.onopen = function () {
+            //chatSocket.send(JSON.stringify({'message' : "Hola Mundo desde el cliente"}))
+            console.log("WS abierto")
+            my.msj="Conectado!"
+        }
+        alertSocket.onmessage = function (data) {
+           console.log(data)
+        }
+    },
+}
+</script>
